b66d864cb938fc48c384fcd04081f70c8198fbbc
hoeyoung2 <2hy03@naver.com>
2024-04-19 01:28:05 +0900

    #comment_[level 0] 다음에 올 숫자

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,8 +1,9 @@
-def solution(common):
+# 조건문에 조건을 조심하기.
+##등비수열일 경우로 조건을 준다면 런타임에러 발생
 
-    if (common[1]-common[0]) == (common[2]-common[1]):
+def solution(common):
+    if (common[1]-common[0]) == (common[2]-common[1]):  #등차수열 이라면,
         answer=(common[-1]+common[1]-common[0])
     else:
-        
         answer= (common[-1]* (common[1]//common[0]))
     return answer
