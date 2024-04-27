7ad0dfcc17f9d9c32c518da7ab873e5a7171cf45
Hoeyoung Lee <101242683+2hy03@users.noreply.github.com>
2024-04-28 00:04:51 +0900

    [Silver I] Title: 그림, Time: 344 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,33 +1,38 @@
-from collections import deque
+#1926.물통
+from collections import deque    
+import sys
+input=sys.stdin.readline
 
-def bfs(x, y):
-    a[x][y] = 0
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    w = 1
-    q = deque()
-    q.append([x, y])
+def bfs(x,y):
+    graph[x][y]=0
+    area=1
+    q=deque()
+    q.append((x,y))
+    
     while q:
-        x, y = q.popleft()
+        x,y=q.popleft()
+        dx=[-1,1,0,0]
+        dy=[0,0,-1,1]
         for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if 0 <= nx < n and 0 <= ny < m and a[nx][ny] == 1:
-                q.append([nx, ny])
-                a[nx][ny] = 0
-                w += 1
-    return w
+            nx=x+dx[i]
+            ny=y+dy[i]
+            
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==1:
+                q.append((nx,ny))
+                area+=1
+                graph[nx][ny]=0
+    return area
 
+n,m=map(int,input().split())
+graph= [list(map(int, input().split())) for _ in range(n)]
 
-n, m = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
-ans = 0
+cnt=0
+max_area=0
 for i in range(n):
-    for j in range(m):
-        if a[i][j] == 1:
-            cnt += 1
-            ans = max(bfs(i, j), ans)
-
+        for j in range(m):
+            if graph[i][j]==1:
+                cnt+=1
+                max_area = max(bfs(i, j), max_area)
+            
 print(cnt)
-print(ans)
+print(max_area)
