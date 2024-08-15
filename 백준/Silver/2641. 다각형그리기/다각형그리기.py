cf32fa6bb357bb5964da2a6317290be2c71be646
Hoeyoung Lee <101242683+2hy03@users.noreply.github.com>
2024-08-16 02:07:20 +0900

    [Silver II] Title: 다각형그리기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2641. 다각형그리기/다각형그리기.py
+++ 백준/Silver/2641. 다각형그리기/다각형그리기.py
@@ -1,26 +1,23 @@
-#2641.다각형그리기
-N=int(input())
-data=list(map(int,input().split()))
+N = int(input())
+data = list(map(int, input().split()))
 
-#반대방향(숫자를 바꾸면서, 순서도 바뀜!!)
-reversemap={1:3,3:1,2:4,4:2}
-data_R=list(map(lambda x : reversemap.get(x,x),data[::-1]))
+# 반대 방향(숫자를 바꾸면서, 순서도 바뀜)
+reversemap = {1: 3, 3: 1, 2: 4, 4: 2}
+data_R = [reversemap[x] for x in reversed(data)]
 
-M=int(input())
-result=[]
+M = int(input())
+result = []
 
 for _ in range(M):
-    c=list(map(int,input().split()))
-    c1=c.copy()
-    
+    c = list(map(int, input().split()))
+    original_c = c[:]  # c의 원본을 복사
+
     for _ in range(N):
-        if data==c or data_R==c:
-            result.append(c1)
+        if c == data or c == data_R:
+            result.append(original_c)
             break
-        else:          
-            c.append(c.pop(0))
+        c.append(c.pop(0))  # 리스트 회전
 
 print(len(result))
-for i in range(len(result)):
-    print(*result[i])  
-        
+for r in result:
+    print(*r)
