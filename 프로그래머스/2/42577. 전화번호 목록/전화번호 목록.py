d5d8be9ae790fe37196e9de4c0fbcdfd60652163
Hoeyoung Lee <101242683+2hy03@users.noreply.github.com>
2024-09-05 23:32:56 +0900

    [level 2] Title: 전화번호 목록, Time: 92.47 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,11 +1,7 @@
 def solution(phone_book):
+    answer = True
     phone_book.sort()
-    for i in range(1,len(phone_book)):
-        msg1=phone_book[i-1]
-        msg2=phone_book[i]
-        if msg2.startswith(msg1) == True:
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
             return False
-            break
-        else:
-            answer= True
     return answer
