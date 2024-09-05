b978d8f125a5a93e7169936969dea91be38a6584
Hoeyoung Lee <101242683+2hy03@users.noreply.github.com>
2024-09-05 23:35:09 +0900

    [level 2] Title: 전화번호 목록, Time: 99.51 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,11 @@
 def solution(phone_book):
-    answer = True
+    # 전화번호 목록을 사전순으로 정렬
     phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
+
+    # 정렬된 상태에서 인접한 번호만 비교
+    for i in range(len(phone_book) - 1):
+        # 다음 번호가 현재 번호의 길이만큼의 접두사인지 확인
+        if phone_book[i + 1][:len(phone_book[i])] == phone_book[i]:
             return False
-    return answer
+
+    return True
